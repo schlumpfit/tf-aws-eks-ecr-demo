e608927f0f7a8657ce3c395a1585fda84645917f
Paul <github@paulfranze.de>
2023-03-13 12:38:04 +0100

    Enable DNS support for vpc

--- terraform/1-vpc.tf
+++ terraform/1-vpc.tf
@@ -1,6 +1,9 @@
 resource "aws_vpc" "eks_demo" {
   cidr_block = "10.0.0.0/16"
 
+  enable_dns_support   = true # Default is already true, but make it explcit.
+  enable_dns_hostnames = true
+
   tags = {
     Name = "eks_demo"
   }
