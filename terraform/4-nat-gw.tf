e15cd697655d60e7477af709a7b42021638fbd74
Paul <github@paulfranze.de>
2023-03-13 14:51:21 +0000

    Add explicit dependency between eip and nat

--- terraform/4-nat-gw.tf
+++ terraform/4-nat-gw.tf
@@ -1,10 +1,15 @@
 resource "aws_eip" "eks_demo_nat_ip" {
   vpc = true
 
+  depends_on = [
+    aws_internet_gateway.eks_demo_public
+  ]
+
   tags = {
     Name = "eks_demo_nat_ip"
   }
 }
+
 resource "aws_nat_gateway" "eks_demo_nat" {
   allocation_id = aws_eip.eks_demo_nat_ip.id
   subnet_id     = aws_subnet.eks_demo_eu_central_1a_public.id
